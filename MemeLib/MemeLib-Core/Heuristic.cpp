58c450c58599be32e2e224583775b33a58d99793
Tyler <tyler.chermely@mymail.champlain.edu>
2016-10-23 18:14:08 -0400

    Fixed heuristic

--- MemeLib/MemeLib-Core/Heuristic.cpp
+++ MemeLib/MemeLib-Core/Heuristic.cpp
@@ -18,5 +18,6 @@ float Heuristic::getEstimate(Node * testNode)
 	glm::vec3 secondLoc = testNode->getPos();
 	float dx = abs(secondLoc.x - mpGoalNode->getPos().x);
 	float dy = abs(secondLoc.y - mpGoalNode->getPos().y);
-	return (dx + dy) + float(sqrt(2) - 2) * min(dx, dy);
+	float dz = abs(secondLoc.z - mpGoalNode->getPos().z);
+	return (dx + dy + dz) + float(sqrt(2) - 2) * min(dx, dy, dz);
 }

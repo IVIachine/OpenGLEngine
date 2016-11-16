92efabbcc9331739db1f75fb3e59e15345395e4a
Adam Gurman <gurman8r@gmail.com>
2016-11-15 21:53:01 -0500

    PongApp

--- MemeLib/Common/PongApp.cpp
+++ MemeLib/Common/PongApp.cpp
@@ -16,10 +16,12 @@ PongApp::~PongApp()
 
 bool PongApp::setup()
 {
+	// Setup Graphics
 	GRAPHICS->getCamera()->setPosition({ 0, 0, -10 });
 	GRAPHICS->getCamera()->setForward({ 0, 0, 1 });
 	GRAPHICS->enableMouseLook(false);
 
+	// Load Resources
 	Shader* p_shader = RESOURCES->addShader("basic", "../Assets/shaders/basicShader");
 
 	RESOURCES->addTexture("paddle1", "../Assets/textures/Pong/paddleBlue.png");
@@ -34,16 +36,17 @@ bool PongApp::setup()
 	RESOURCES->addSprite("paddle2", RESOURCES->getTexture2D("paddle2"));
 	RESOURCES->addSprite("ball", RESOURCES->getTexture2D("ball"));
 
+	// Create Paddles and Ball
 	m_paddle1.sprite = RESOURCES->getSprite("paddle1");
-	m_paddle1.sprite->setScale(Vec3(0.25f, 1.0f, 0.25f));
+	m_paddle1.sprite->setScale(Vec3(0.23f, 1.0f, 1.f));
 	m_paddle1.sprite->setRotation(Vec3(0, 180, 0) * Maths::DEG_TO_RAD);
 
 	m_paddle2.sprite = RESOURCES->getSprite("paddle2");
-	m_paddle2.sprite->setScale(Vec3(0.25f, 1.0f, 0.25f));
+	m_paddle2.sprite->setScale(Vec3(0.23f, 1.0f, 1.f));
 	m_paddle2.sprite->setRotation(Vec3(0, 180, 0) * Maths::DEG_TO_RAD);
 
 	m_ball.sprite = RESOURCES->getSprite("ball");
-	m_ball.sprite->setScale(Vec3(0.25f, 0.25f, 0.25f));
+	m_ball.sprite->setScale(Vec3(0.211f, 0.211f, 1.f));
 	m_ball.sprite->setRotation(Vec3(0, 180, 0) * Maths::DEG_TO_RAD);
 
 	m_paddle1.position = Vec3(0.f, 0.f, 0.f);
@@ -59,10 +62,9 @@ void PongApp::clear()
 
 void PongApp::update()
 {
-	Camera* cam = GRAPHICS->getCamera();
-
-	m_paddle1.position = { -1, 0, 0 };
-	m_paddle2.position = { 1, 0, 0 };
+	// Set positions
+	m_paddle1.position = { -5, 0, 0 };
+	m_paddle2.position = { 5, 0, 0 };
 	m_ball.position = { 0, 0, 0 };
 
 	m_paddle1.sprite->setPosition(m_paddle1.position);
@@ -72,6 +74,15 @@ void PongApp::update()
 	if (INPUT->getKeyDown(Keyboard::KeyCode::Escape))
 	{
 		GAME->stop();
+	}
+
+	if (INPUT->getKey(Keyboard::Up))
+	{
+		// Send move up
+	}
+	else if (INPUT->getKey(Keyboard::Down))
+	{
+		// Send move down
 	}
 }
 
